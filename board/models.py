7191154dc4334e71b5338d2d04c396f814c253c9
Sourav Das <iamsouravdaswade420@gmail.com>
2024-12-21 19:51:05 +0530

    first commit

---
+++ board/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.contrib.auth.models import  User
+
+
+class Board(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,related_name="boards")
+
+    def __str(self):
+        return  f"Board: {self.name}"
+
+
+
