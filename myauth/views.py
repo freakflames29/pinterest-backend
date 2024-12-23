6e7baf70de57c44205b06d8793391a63f0cd4d73
Sourav Das <iamsouravdaswade420@gmail.com>
2024-12-23 13:10:17 +0530

    change pin creation post method

--- myauth/views.py
+++ myauth/views.py
@@ -8,19 +8,24 @@ from django.contrib.auth.models import User
 
 class UserView(APIView):
     def post(self, rq):
-        username = rq.data['username']
-        email = rq.data['email']
-        password = rq.data['password']
+        try:
+            username = rq.data['username']
+            email = rq.data['email']
+            password = rq.data['password']
 
-        user = User(username=username, email=email)
-        user.set_password(password)
-        user.save()
-        token = str(AccessToken.for_user(user))
 
-        data = {
-            "id": user.id,
-            "username": user.username,
-            "email": user.email,
-            "token": token
-        }
-        return Response(data, status=status.HTTP_200_OK)
+
+            user = User(username=username, email=email)
+            user.set_password(password)
+            user.save()
+            token = str(AccessToken.for_user(user))
+
+            data = {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "token": token
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        except:
+            return  Response({"msg":"Username, password , email required"},status=400)
--- pin/views.py
+++ pin/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
-from rest_framework import  generics
-from .serializers import  PinSerializer
-from .models import  Pin
+from rest_framework import generics
+from .serializers import PinSerializer
+from .models import Pin
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import  Response
+from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework import  mixins
 
 class PinListView(generics.ListAPIView):
     queryset = Pin.objects.all()
@@ -16,13 +17,19 @@ class PinListView(generics.ListAPIView):
     search_fields = ["title"]
 
 
-class PinCreateView(APIView):
+class PinCreateView(generics.CreateAPIView):
     permission_classes = [IsAuthenticated]
     authentication_classes = [JWTAuthentication]
-    def post(self,rq):
-        pin_ser = PinSerializer(data=rq.data)
-        if pin_ser.is_valid():
-            pin_ser.save(user = rq.user)
-            return  Response(pin_ser.data,status=201)
-        else:
-            return  Response(pin_ser.errors,status=400)
+    serializer_class = PinSerializer
+    queryset = Pin.objects.all()
+
+    # def post(self, rq):
+    #     pin_ser = PinSerializer(data=rq.data)
+    #     if pin_ser.is_valid():
+    #         pin_ser.save(user=rq.user)
+    #         return Response(pin_ser.data, status=201)
+    #     else:
+    #         return Response(pin_ser.errors, status=400)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
