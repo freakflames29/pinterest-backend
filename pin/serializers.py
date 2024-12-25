9c0bcca708a928dbdb476ca34eb6203ca35875ce
Sourav Das <iamsouravdaswade420@gmail.com>
2024-12-25 19:28:51 +0530

    Added username  in pin list

--- pin/serializers.py
+++ pin/serializers.py
@@ -1,9 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer,SerializerMethodField
 from .models import Pin
 
 
 class PinSerializer(ModelSerializer):
+    username= SerializerMethodField(read_only=True)
+    
+    def get_username(self,obj):
+        return obj.user.username
+    
     class Meta:
         model = Pin
-        fields = "__all__"
+        fields = ["id","title","link","desc","image","user","username","boards"]
         read_only_fields = ["user"]
