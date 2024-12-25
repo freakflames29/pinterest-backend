8f4b9880b6c9135f0009eb0a018b467f994bb225
Sourav Das <iamsouravdaswade420@gmail.com>
2024-12-25 20:54:44 +0530

    added middlware for delay in response

--- backend/settings.py
+++ backend/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'pin.middleware.DelayMiddleware'
 ]
 CORS_ALLOW_ALL_ORIGINS = True
 
---
+++ pin/middleware.py
@@ -0,0 +1,11 @@
+import time
+class DelayMiddleware:
+    def __init__(self,get_response):
+        self.get_response = get_response
+        
+    def __call__(self,rq):
+        response = self.get_response(rq)
+        time.sleep(1)
+        return response
+        
+        
