af0183b2667c729a7ef431c41cc66a023898df90
Sourav Das <iamsouravdaswade420@gmail.com>
2025-01-16 14:08:42 +0530

    Added banner image to category

--- category/models.py
+++ category/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
+    banner = models.ImageField(upload_to="banner/",blank=True)
     
     def __str__(self):
         return self.title
