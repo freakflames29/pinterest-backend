12a4390b97c5f8d3e8ab08763e4000bb93e36810
Sourav Das <iamsouravdaswade420@gmail.com>
2025-01-16 12:58:39 +0530

    Added category pins

--- category/urls.py
+++ category/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.CategoryListView.as_view())
+    path("",views.CategoryListView.as_view()),
+    path("<int:pk>/pins/",views.CategorySinglePinView.as_view()),
 ]
--- category/views.py
+++ category/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .serializers import CategorySerializer
 from .models import Category
+from rest_framework.views import APIView
+from pin.serializers import PinSerializer
+from rest_framework.response import Response
 
 from rest_framework import generics
 
@@ -8,3 +11,13 @@ class CategoryListView(generics.ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
+class CategorySinglePinView(APIView):
+    def get(self,rq,pk):
+        try:
+            cat = Category.objects.get(pk=pk)
+            pins = cat.pins.all()
+            pinser= PinSerializer(pins,many=True,context={"request":rq})
+            return Response(pinser.data,status=200)
+        except Exception as e:
+            print("x"*100,e)
+            return Response({"error":"No category found"},status=404)
